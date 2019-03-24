0b73ae0886a44d285b6b06c2ee4538633b9c3603
Yash Modi <myn.yashnm@gmail.com>
2019-03-24 12:56:08 +0530

    Modified LoginActivity with Password reset
    I have added resetPassword functionality which is working fine. Don't forget to change resetScript.html as that file is also modified to work with resetPassword().
    
    I have also implemented AsyncTask to do login process in background.

--- LoginActivity.java
+++ LoginActivity.java
@@ -1,7 +1,12 @@
 package project.airved;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -16,13 +21,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.mongodb.stitch.android.core.StitchAppClient;
-import com.mongodb.stitch.android.core.auth.providers.userpassword.UserPasswordAuthProviderClient;
-import com.mongodb.stitch.core.auth.providers.userpassword.UserPasswordCredential;
 import com.mongodb.stitch.android.core.Stitch;
 import com.mongodb.stitch.android.core.auth.StitchUser;
+import com.mongodb.stitch.android.core.auth.providers.userpassword.UserPasswordAuthProviderClient;
+import com.mongodb.stitch.core.auth.providers.userpassword.UserPasswordCredential;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     private EditText emailView;
     private EditText passwordView;
@@ -30,15 +35,15 @@ public class LoginActivity extends AppCompatActivity {
     private TextView registerLink;
     private TextView resetPassword;
     private StitchUser currentUser;
-    public static StitchAppClient client;
+
     @Override
     protected void onStart() {
         super.onStart();
-        if(client == null)
-            client = Stitch.initializeAppClient("YOUR STITCH APP_ID");
-
-        client = Stitch.getAppClient("YOUR STITCH APP_ID");
-        currentUser = client.getAuth().getUser();
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+        Splash.client = Stitch.getAppClient("YOUR APP-ID");
+        currentUser = Splash.client.getAuth().getUser();
         if(currentUser != null){
             startActivity(new Intent(LoginActivity.this, Dashboard.class));
             finish();
@@ -76,7 +81,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        resetPassword.setOnClickListener(new View.OnClickListener(){
+        resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 recoverPassword();
@@ -116,46 +121,80 @@ public class LoginActivity extends AppCompatActivity {
         if (cancel) {
             focusView.requestFocus();
         } else {
-            UserPasswordCredential credential = new UserPasswordCredential(email, password);
-            client.getAuth().loginWithCredential(credential).addOnCompleteListener(new OnCompleteListener<StitchUser>() {
-                @Override
-                public void onComplete(@NonNull final Task<StitchUser> task) {
-                    if (task.isSuccessful()) {
-                        Log.d("stitch", "Successfully logged in as user " + task.getResult().getId());
-                        Toast.makeText(LoginActivity.this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this, Dashboard.class));
-                        finish();
-                    } else {
-                        Log.e("stitch", "Error logging in with email/password auth:", task.getException());
-                        Toast.makeText(LoginActivity.this, "Unsuccessful login.", Toast.LENGTH_SHORT).show();
-                        passwordView.setText("");
+            //UserPasswordCredential credential = new UserPasswordCredential(params[0], params[1]);
+            new getLoginTask().execute(email,password);
+        }
+    }
+
+    private class getLoginTask extends AsyncTask<String, Void, Void> {
+        private ProgressDialog pd;
+        private byte statusCode;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = ProgressDialog.show(LoginActivity.this, "", "Airis is recognizing you...", true, false); // Create and show Progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                UserPasswordCredential credential = new UserPasswordCredential(params[0], params[1]);
+                Splash.client.getAuth().loginWithCredential(credential).addOnCompleteListener(new OnCompleteListener<StitchUser>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<StitchUser> task) {
+                        if (task.isSuccessful()){
+                            statusCode = 1;
+                            onPostExecute();
+                        }
+                        else{
+                            statusCode = 0;
+                            onPostExecute();
+                        }
                     }
-                }
-            });
+                });
+            } catch (Exception e) {
+                Log.e("stitch-auth", "Authentication Failed!");
+            }
+            return null;
+        }
+
+        protected void onPostExecute() {
+            if (statusCode == 1) {
+                Log.d("stitch-auth", "Authentication Successful.");
+                Toast.makeText(LoginActivity.this, "Airis welcomes you!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, Dashboard.class));
+                pd.dismiss();
+                finish();
+            } else {
+                Log.e("stitch-auth", "Authentication Failed.");
+                pd.dismiss();
+                Toast.makeText(LoginActivity.this, "Ugh...! Error in recognizing.", Toast.LENGTH_SHORT).show();
+                passwordView.setText("");
+            }
         }
     }
 
     private void recoverPassword() {
-        View focusView = null;
         String email = emailView.getText().toString();
+        UserPasswordAuthProviderClient emailPassClient = Stitch.getDefaultAppClient().getAuth().getProviderClient(UserPasswordAuthProviderClient.factory);
         if (TextUtils.isEmpty(email)) {
             emailView.setError(getString(R.string.error_field_required));
-            focusView = emailView;
         } else {
-            UserPasswordAuthProviderClient emailPassClient = client.getAuth().getProviderClient(UserPasswordAuthProviderClient.factory);
-
-            emailPassClient.sendResetPasswordEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull final Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        Log.d("stitch", "Password reset mail sent.");
-                        Toast.makeText(LoginActivity.this, "Password reset mail sent.", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Log.e("stitch", "Error sending password reset email:", task.getException());
-                        Toast.makeText(LoginActivity.this, "Account not found!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-            }
+            emailPassClient.sendResetPasswordEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                               @Override
+                                               public void onComplete(@NonNull final Task<Void> task) {
+                                                   if (task.isSuccessful()) {
+                                                       Log.d("stitch-auth", "Successfully sent password reset email");
+                                                       Toast.makeText(LoginActivity.this, "Password reset mail sent.", Toast.LENGTH_SHORT).show();
+                                                   } else {
+                                                       Log.e("stitch-auth", "Error sending password reset email:", task.getException());
+                                                       Toast.makeText(LoginActivity.this, "Account not found!", Toast.LENGTH_SHORT).show();
+                                                   }
+                                               }
+                                           }
+                    );
         }
+    }
 }
